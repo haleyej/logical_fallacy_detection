76cbc6503777b4a45cb42e354a4e69fad916d18f
haleyej <haleyej@umich.edu>
2024-04-23 22:44:05 -0400

    more tweaks

--- models/evaluate_misinfo_detection.py
+++ models/evaluate_misinfo_detection.py
@@ -15,18 +15,26 @@ from liar_fine_tuning import LiarDataset
 #to run 
 #python3 evaluate_misinfo_detection.py  --data_path='../data/liar_dataset/test.tsv' --model_type='pretrained' --model_checkpoint='../pretrained_models/liar_output_snli/checkpoint-16000'
 
-os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
-
-# set metrics
 # set metrics
 accuracy = evaluate.load("accuracy")
 f1 = evaluate.load("f1")
 
 def compute_metrics(predictions:list[int], labels:list[int]) -> dict:
+    '''
+    helper function to compute evaluation metrics
+
+    ARGUMENTS: 
+        predictions: list of binary class predictions from model 
+        labels: list of true binary labels 
+
+    RETURNS:
+        dictionary mapping evaluation metrics to their values
+    '''
     f1_macro = f1.compute(predictions=predictions, references=labels, average='macro')['f1']
     f1_weighted = f1.compute(predictions=predictions, references=labels, average='weighted')['f1']
     accuracy_score = accuracy.compute(predictions=predictions, references=labels)['accuracy']
     return {'accuracy':accuracy_score, 'f1_macro':f1_macro, 'f1_balanced':f1_weighted}
+
 
 def evaluate_model(data_path:str, 
                    save_path:str=os.getcwd(), 
@@ -40,7 +48,6 @@ def evaluate_model(data_path:str,
     tokenizer = BertTokenizerFast.from_pretrained('google-bert/bert-base-uncased')
 
     model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint)
-
     model = model.to(device)
     model.eval()
 
@@ -70,14 +77,14 @@ def evaluate_model(data_path:str,
     scores = compute_metrics(all_predictions, all_labels)
 
     with open(os.path.join(save_path, f'{model_type}_evaluation.csv'), 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fields)
-        writer.writeheader(['metric', 'value'])
-        writer.writerows(scores)
+        writer = csv.writer(csvfile)
+        writer.writerow(['metric', 'value'])
+        for metric, value in scores.items():
+            writer.writerow([metric, value])
 
     
 
 def main(args):
-    print(args)
     evaluate_model(args['data_path'], 
                    args['save_path'],
                    args['model_type'], 
